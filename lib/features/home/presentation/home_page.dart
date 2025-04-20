d2fa8b8b51816664171c74b49869399a6bd09cd2
Maia <gbmmaia.contato@gmail.com>
2025-04-20 18:20:07 -0300

    fix: adicionado barra de pesquisa e melhorado parte visual da home

--- lib/features/home/presentation/home_page.dart
+++ lib/features/home/presentation/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:brasilcard/core/di/core_di.dart';
+import 'package:brasilcard/core/utils/debounce.dart';
+import 'package:brasilcard/core/utils/extensions/widget_extensions.dart';
+import 'package:brasilcard/core/widgets/ds_loading_indicator.dart';
 import 'package:brasilcard/features/coin_list/presentation/viewmodel/coin_list/coin_list_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
@@ -12,6 +15,8 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late ICoinListViewModel viewmodel;
+  final controller = TextEditingController();
+  final debounce = Debounce(milliseconds: 1000);
 
   @override
   void initState() {
@@ -23,25 +28,46 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Text('BrasilCard')),
       backgroundColor: Colors.white,
-      body: Observer(
-        builder: (context) {
-          return viewmodel.isLoading
-              ? CircularProgressIndicator()
-              : viewmodel.errorMessage != null
-              ? Center(child: Text(viewmodel.errorMessage!))
-              : ListView.separated(
-                itemCount: viewmodel.cryptos.length,
-                separatorBuilder: (context, index) => Divider(),
-                itemBuilder: (context, index) {
-                  final model = viewmodel.cryptos[index];
-                  return Text(
-                    model.name,
-                    style: TextStyle(color: Colors.black),
-                  );
-                },
-              );
-        },
+      body: Column(
+        children: [
+          SearchBar(
+            elevation: WidgetStatePropertyAll(0),
+            controller: controller,
+            onChanged: (value) {
+              debounce(() => viewmodel.getCryptos(query: value));
+            },
+          ).all(16),
+          Expanded(
+            child: Observer(
+              builder: (context) {
+                if (viewmodel.isLoading) {
+                  return DSLoadingIndicator();
+                }
+                if (viewmodel.errorMessage != null) {
+                  return Center(child: Text(viewmodel.errorMessage!));
+                }
+
+                return Visibility(
+                  visible: viewmodel.cryptos.isEmpty,
+                  replacement: Text('Nenhuma criptomoeda encontrada'),
+                  child: ListView.separated(
+                    itemCount: viewmodel.cryptos.length,
+                    separatorBuilder: (context, index) => Divider(),
+                    itemBuilder: (context, index) {
+                      final model = viewmodel.cryptos[index];
+                      return Text(
+                        model.name,
+                        style: TextStyle(color: Colors.black),
+                      );
+                    },
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
