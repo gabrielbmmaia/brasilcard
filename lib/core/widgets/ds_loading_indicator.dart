fe30a6cae55157ceab077c8e4a468c9c549f2061
Maia <gbmmaia.contato@gmail.com>
2025-04-20 18:18:55 -0300

    feat: criado loading indicator padrão do projeto

---
+++ lib/core/widgets/ds_loading_indicator.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class DSLoadingIndicator extends StatelessWidget {
+  const DSLoadingIndicator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SizedBox(
+        height: 36.h,
+        width: 36.w,
+        child: CircularProgressIndicator(color: Colors.orange, strokeWidth: 3),
+      ),
+    );
+  }
+}
