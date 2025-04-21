bb070d99552f05794dd1ab6b2a856bad63f5f4d4
Maia <gbmmaia.contato@gmail.com>
2025-04-21 15:13:58 -0300

    fix: alterado size_extension de double para num

--- lib/core/utils/extensions/size_extensions.dart
+++ lib/core/utils/extensions/size_extensions.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-extension DoubleExt on double {
+extension DoubleExt on num {
   SizedBox get hg => SizedBox(height: toDouble().h);
 
   SizedBox get wt => SizedBox(width: toDouble().w);
