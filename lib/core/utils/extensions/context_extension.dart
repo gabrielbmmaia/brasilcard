4c79948759ee2deee537dcc4c4b5f41334b5f1c3
Maia <gbmmaia.contato@gmail.com>
2025-04-22 14:55:33 -0300

    feat: criado snackbar padrão do projeto direto na context.extension

--- lib/core/utils/extensions/context_extension.dart
+++ lib/core/utils/extensions/context_extension.dart
@@ -1,5 +1,23 @@
+import 'package:brasilcard/core/theme/text_style.dart';
+import 'package:brasilcard/core/widgets/ds_text.dart';
 import 'package:flutter/material.dart';
 
 extension ContextExt on BuildContext {
   ColorScheme get colorTheme => Theme.of(this).colorScheme;
+
+  void showSimpleSnackBar({required String text, Duration? duration}) =>
+      ScaffoldMessenger.of(this)
+        ..removeCurrentSnackBar()
+        ..showSnackBar(
+          SnackBar(
+            content: DSText(
+              text,
+              style: AppTextStyle.h6,
+              color: colorTheme.primary,
+            ),
+            backgroundColor: colorTheme.tertiary,
+
+            duration: duration ?? const Duration(seconds: 2),
+          ),
+        );
 }
